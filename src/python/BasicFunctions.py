2201f84ac707ce12ab0217e62a2f8bc72964ffe0
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-11-01 11:18:21 -0800

    added basisFourier() to take top k Fourier basis vectors and a test

--- src/python/BasicFunctions.py
+++ src/python/BasicFunctions.py
@@ -2,4 +2,28 @@
 Helper functions for compressed_sensing.
 """
 
-def imread()
+import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+def imread(imfile):
+    """ Read image from file and normalize. """
+    
+    img = mpimg.imread(imfile).astype(np.float)
+    img = rescale(img)
+    return img
+
+def imshow(img, title="", cmap="gray", cbar=False):
+    """ Show image to screen. """
+    
+    plt.imshow(img, cmap=cmap)
+    plt.title(title)
+    
+    if cbar:
+        plt.colorbar()
+        plt.show()
+        
+def imsave(img, imfile):
+    """ Save image to file."""
+    
+    mpimg.imsave(imfile, img)
