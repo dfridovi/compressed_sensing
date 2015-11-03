d891ae447416426766d4f184aa00eba91d6671f3
Grace Kuo <gracekuo01@gmail.com>
2015-11-02 17:39:21 -0800

    Added template for sketching test

---
+++ src/python/test_basis_sketching_1D.py
@@ -0,0 +1,10 @@
+"""
+Test script. Run Fourier decomposition on a 2D image and see what we get.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import  misc
+import BasicFunctions as bf
+import Sketching as sketch
+
