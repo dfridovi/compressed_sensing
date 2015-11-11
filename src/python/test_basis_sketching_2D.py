836619032212539c541b4c78d061676ce45a1535
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2015-11-10 18:43:03 -0800

    saves figures now in sketching_2d

--- src/python/test_basis_sketching_2D.py
+++ src/python/test_basis_sketching_2D.py
@@ -11,8 +11,8 @@ import Sketching as sketch
 # Parameters.
 IMAGE_PATH = "../../data/"
 IMAGE_NAME = "lenna.png"
-SIZE = (75, 75)
-ALPHA = 0.1
+SIZE = (250, 250)
+ALPHA = 1.0
 BASIS_OVERSAMPLING = 1.0
 
 # Import the image.
@@ -31,8 +31,10 @@ plt.imshow(reconstruction, cmap="gray")
 max_value = np.absolute(coefficients).max()
 plt.title(("Reconstruction using %d random basis vectors in image domain." %
            (np.absolute(coefficients) > 0.01 * max_value).sum()))
-plt.show()
+plt.savefig("sketching_%1.1f_l1_%d_reconstruction.png" % (ALPHA, SIZE[0]))
+
 
 plt.figure(2)
 plt.hist(np.absolute(coefficients), bins=len(coefficients) / 10)
+plt.savefig("sketching_%1.1f_l1_%d_histogram.png" % (ALPHA, SIZE[0]))
 plt.show()
