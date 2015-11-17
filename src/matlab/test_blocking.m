55eafadb0a2f6c819279c1cc8234e71b6541100b
Grace Kuo <gracekuo01@gmail.com>
2015-11-17 14:59:55 -0800

    really fixed blocking

--- src/matlab/test_blocking.m
+++ src/matlab/test_blocking.m
@@ -4,13 +4,15 @@
 % Parameters 
 IMAGE_PATH = '../../data/';
 IMAGE_NAME = 'lenna.png';
-BLOCK_SIZE = 10;
+BLOCK_SIZE = 40;
 ALPHA = 1.0;
 BASIS_OVERSAMPLING = 1.0;
-OVERLAP_PERCENT = .5;
+OVERLAP_PERCENT = .4;
 
 img = imread([IMAGE_PATH IMAGE_NAME]);
 img = rgb2gray(img);
+
+%img = ones(size(img));
 
 blocks = getBlocks(img,BLOCK_SIZE, OVERLAP_PERCENT);
 img_reassembled = assembleBlocks(blocks, BLOCK_SIZE, size(img), OVERLAP_PERCENT);
